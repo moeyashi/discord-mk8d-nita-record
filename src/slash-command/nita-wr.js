dd7e868853f9b62c264622b7ff508566ec97930c
Ren Adachi <49895682+moeyashi@users.noreply.github.com>
2024-01-20 19:08:19 +0900

    feat: add /nita-wr #37

---
+++ src/slash-command/nita-wr.js
@@ -0,0 +1,41 @@
+// @ts-check
+import { SlashCommandBuilder } from 'discord.js';
+import { searchTrack } from '../const/track.js';
+import { displayMilliseconds } from '../util/time.js';
+
+/** @type { import('../types').SlashCommand } */
+export default {
+  data: new SlashCommandBuilder()
+    .setName('nita-wr')
+    .setDescription('NITAのWRタイムを確認します。')
+    .addStringOption(option => option.setName('track').setDescription('コース名').setRequired(true)),
+  execute: async (interaction) => {
+    const trackQuery = interaction.options.getString('track');
+
+    if (!trackQuery) {
+      throw new Error('コース名を指定してください');
+    }
+
+    const track = searchTrack(trackQuery);
+    if (!track) {
+      throw new Error(`コースが見つかりませんでした。\n入力されたコース名:  ${trackQuery}`);
+    }
+
+    await interaction.reply({
+      embeds: [{
+        title: `${track.trackName}のWR`,
+        fields: [
+          {
+            name: 'VSカスタム',
+            value: displayMilliseconds(track.nitaVSWRMilliseconds),
+          },
+          {
+            name: '全カスタム',
+            value: displayMilliseconds(track.nitaAllCombinationWRMilliseconds),
+          },
+        ],
+      }],
+    });
+    return;
+  },
+};
