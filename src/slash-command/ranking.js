2dd1617a907462b001fccc32e2fbe3577b18ff3b
Ren Adachi <49895682+moeyashi@users.noreply.github.com>
2024-02-24 12:30:55 +0900

    feat: rankingのembedsをnita-listとおなじように分ける
    Signed-off-by: Ren Adachi <49895682+moeyashi@users.noreply.github.com>

--- src/slash-command/ranking.js
+++ src/slash-command/ranking.js
@@ -2,7 +2,8 @@
 import { SlashCommandBuilder } from 'discord.js';
 import { searchTrack } from '../const/track.js';
 import { planetScaleRepository } from '../infra/repository/planetscale.js';
-import { ceilDiff, displayMilliseconds } from '../util/time.js';
+import { displayMilliseconds } from '../util/time.js';
+import { colorByTimeRank } from '../const/color.js';
 
 /** @type { import('../types').SlashCommand } */
 export default {
@@ -43,23 +44,63 @@ export default {
       return;
     }
 
-    /** @type {import('discord.js').APIEmbed[]} */
-    const embeds = [];
+    const groupedRanking = groppByRank(track, ranking);
+
     await interaction.followUp({
-      embeds: ranking.reduce((pv, cv, i) => {
-        if (i % 25 === 0) {
-          pv.push({
-            title: `${track.trackName}のNITAランキング`,
-            fields: [],
+      content: `NITAランキング - ${track.trackName}`,
+      embeds: groupedRanking.flatMap(([rank, color, nita]) => {
+        /** @type {import('discord.js').APIEmbed[]} */
+        const embeds = [];
+        return nita.reduce((pv, cv, i) => {
+          if (i % 25 === 0) {
+            pv.push({
+              title: `${rank}`,
+              color,
+              fields: [],
+            });
+          }
+          const diffWRnNita = cv.milliseconds - track.nitaVSWRMilliseconds;
+          pv[pv.length - 1].fields?.push({
+            name: cv.member.nickname || cv.member.user.username,
+            value: `${displayMilliseconds(cv.milliseconds)} WR + ${diffWRnNita / 1000}秒`,
           });
-        }
-        pv[pv.length - 1].fields?.push({
-          name: cv.member.nickname || cv.member.user.username || 'unknown',
-          value: `${displayMilliseconds(cv.milliseconds)} ${ceilDiff(track.nitaVSWRMilliseconds, cv.milliseconds)}落ち`,
-        });
-        return pv;
-      }, embeds),
+          return pv;
+        }, embeds);
+      }),
     });
-    return;
   },
+};
+
+/**
+ * @param {import('../types').Track} track
+ * @param {Array<{ member: import('discord.js').GuildMember, milliseconds: number }>} ranking
+ * @returns {Array<[string, number|undefined, Array<{ member: import('discord.js').GuildMember, milliseconds: number }>]>}
+ */
+const groppByRank = (track, ranking) => {
+  /** @type {Array<[string, number|undefined, Array<{ member: import('discord.js').GuildMember, milliseconds: number }>]>} */
+  const ret = [
+    ['1落ち', colorByTimeRank(1), []],
+    ['2落ち', colorByTimeRank(2), []],
+    ['3落ち', colorByTimeRank(3), []],
+    ['4落ち', colorByTimeRank(4), []],
+    ['5落ち', colorByTimeRank(5), []],
+    ['6落ち以上', colorByTimeRank(6), []],
+  ];
+  ranking.forEach((nita) => {
+    const diffWRnNita = nita.milliseconds - track.nitaVSWRMilliseconds;
+    if (diffWRnNita < 1000) {
+      ret[0][2].push(nita);
+    } else if (diffWRnNita < 2000) {
+      ret[1][2].push(nita);
+    } else if (diffWRnNita < 3000) {
+      ret[2][2].push(nita);
+    } else if (diffWRnNita < 4000) {
+      ret[3][2].push(nita);
+    } else if (diffWRnNita < 5000) {
+      ret[4][2].push(nita);
+    } else {
+      ret[5][2].push(nita);
+    }
+  });
+  return ret.filter(([, , arr]) => arr.length > 0);
 };
